88e10148ba309e6fcfd227fce6a63705ac0690ac
retegrete <ortunomi@live.com>
2022-03-22 14:16:55 -0600

    1

--- myApp.js
+++ myApp.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
+const {MongoClient, ServerApiVersion} = require('mongodb')
+
+
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true})
+
+
+
 
 
 let Person;
